53ab4adc7e6a596591a52bcb60449e43c25e36a0
Akinbo Olamilekan Daniel <olasocials@gmail.com>
2022-07-28 07:26:32 +0100

    images sizes compressed

---
+++ js/alert.js
@@ -0,0 +1,5 @@
+alert(
+`WMN was made with no malicious intent at heart.
+ If you or anyone you know is offended by this
+ kindly reach out and politely state your case.
+ Gracias mi Camarada!`)
---
+++ js/pagesScript.js
@@ -0,0 +1,621 @@
+let fellows = [
+    {
+        fullname: "Abdulhafeez Abdul",
+        url: "images/Abdulhafeez_Abdul.jpg",
+        option1: "Abdulhafeez Abdulkareem",
+        option2: "Musa Ojo",
+        option3: "Abdulhafeez Abdul",
+    },
+    {
+        fullname: "AbdulRahman AbdulSalam",
+        url: "images/AbdulRahman_AbdulSalam.jpeg",
+        option1: "AbdulRahman Aliyu",
+        option2: "Abdul karim",
+        option3: "AbdulRahman AbdulSalam",
+    },
+    {
+        fullname: "Adaeze Ndupu",
+        url: "images/Adaeze_Ndupu.jpg",
+        option1: "Ada Ndupu",
+        option2: "Adaeze Ndupu",
+        option3: "Cynthia Nwankwo"
+    },
+    {
+        fullname: "Adeola Ajibade",
+        url: "images/Adeola_Ajibade.jpeg",
+        option1: "Adeola Adesola",
+        option2: "Zainab Momoh",
+        option3: "Adeola Ajibade",
+    },
+    {
+        fullname: "Adetola Isaiah",
+        url: "images/Adetola_Isaiah.jpeg",
+        option1: "Ajiboye Adetola",
+        option2: "Adetola Isaiah",
+        option3: "Olamilekan Daniel"
+    },
+    {
+        fullname: "Agunbiade Adeyemi",
+        url: "images/Agunbiade_Adeyemi.jpeg",
+        option1: "Agunbiade Adeyemi",
+        option2: "Akinnukawe Adeola",
+        option3: "Amusa Agunbiade",
+    },
+    {
+        fullname: "Ahmed Adenigba",
+        url: "images/Ahmed_Adenigba.jpeg",
+        option1: "Adeola Ajibade",
+        option2: "Ahmed Adeyemi",
+        option3: "Ahmed Adenigba",
+    },
+    {
+        fullname: "Ajiboye Muideen",
+        url: "images/Ajiboye_muideen.jpeg",
+        option1: "Isah Muideen",
+        option2: "Ajiboye Muideen",
+        option3: "Moshood George"
+    },
+    {
+        fullname: "Akinnukawe Gbenga",
+        url: "images/Akinnukawe_Gbenga.jpg",
+        option1: "Akinnukawe Sanusi",
+        option2: "Akinnukawe Gbenga",
+        option3: "Godswill Chibuzo"
+    },
+    {
+        fullname: "Aliyu Isa",
+        url: "images/aliyu_isa.jpeg",
+        option1: "Aliyu Isa",
+        option2: "Akinnukawe Gbenga",
+        option3: "Alihu Hassan",
+    },
+    {
+        fullname: "Amusa Oladimeji",
+        url: "images/amusa oladimeji.jpg",
+        option1: "Amusa George",
+        option2: "AbdulRahman Oladimeji",
+        option3: "Amusa Oladimeji",
+    },
+    {
+        fullname: "Arah Barnabas",
+        url: "images/Arah_Barnabas.jpeg",
+        option1: "Ahmed Adenigba",
+        option2: "Uche Barnabas",
+        option3: "Arah Barnabas",
+    },
+    {
+        fullname: "Ayug Joshua",
+        url: "images/ayug _donald_joshua.jpg",
+        option1: "Ayug Joshua",
+        option2: "Ayug Josiah",
+        option3: "Ahug Donald",
+    },
+    {
+        fullname: "Billiamin Abbas",
+        url: "images/BILLIAMIN_Abbas.jpg",
+        option1: "Abbas Amusa",
+        option2: "Billiamin Abbas",
+        option3: "Amusa Olamilekan",
+    },
+    {
+        fullname: "Bolatito Alaba",
+        url: "images/Bolatito_Alaba.jpeg",
+        option1: "Oma Raymond",
+        option2: "Beatrice Alaba",
+        option3: "Bolatito Alaba",
+    },
+    {
+        fullname: "Bongshim Edward",
+        url: "images/Bongshim_Edward.jpeg",
+        option1: "Godswill Bong",
+        option2: "Bongshim Edward",
+        option3: "Arah Edward",
+    },
+    {
+        fullname: "Chidi Confidence",
+        url: "images/Chidi_Confidence.jpeg",
+        option1: "Billiamin Abbas",
+        option2: "Confidence Nweke",
+        option3: "Chidi Confidence",
+    },
+    {
+        fullname: "Cynthia Nwankwo",
+        url: "images/Cynthia_Nwankwo.jpeg",
+        option2: "Cynthia Nwigwe",
+        option1: "Cynthia Nwankwo",
+        option3: "Cynthia Obi",
+    },
+    {
+        fullname: "Echezona Oji",
+        url: "images/Echezona_Oji.jpeg",
+        option1: "Echezona Oji",
+        option2: "Uche Oji",
+        option3: "Arah Barnabas"
+    },
+    {
+        fullname: "Eduvie Udusegbe",
+        url: "images/Eduvie_Udusegbe.jpeg",
+        option1: "Wilson Eduvie",
+        option2: "Solabi Abraham",
+        option3: "Eduvie Udusegbe",
+    },
+    {
+        fullname: "Farida Sule",
+        url: "images/Farida_Sule.jpeg",
+        option1: "Farida Mogaji",
+        option2: "Farida Sule",
+        option3: "Hauwa Sule",
+    },
+    {
+        fullname: "Godswill Chibuzo",
+        url: "images/Godswill_Chibuzo.jpeg",
+        option1: "Godswill Udoh",
+        option2: "Chibuzo Udoh",
+        option3: "Godswill Chibuzo",
+    },
+    {
+        fullname: "Godswill Udoh",
+        url: "images/Godswill_Udoh.jpeg",
+        option1: "Godswill Udoh",
+        option2: "Godswill Chibuzo",
+        option3: "Chibuzo Udoh",
+    },
+    {
+        fullname: "Hamzat Adejoke",
+        url: "images/Hamzat Adejoke.jpg",
+        option1: "Modinat Adejoke",
+        option2: "Temitope Mogaji",
+        option3: "Hamzat Adejoke",
+    },
+    {
+        fullname: "Hauwa Adamu",
+        url: "images/Hauwa_Adamu.jpeg",
+        option1: "Hauwa Sule",
+        option2: "Hauwa Adamu",
+        option3: "Rotimi Jemilat",
+    },
+    {
+        fullname: "Isaac Oluwatoyin",
+        url: "images/Isaac_Oluwatoyin.jpeg",
+        option1: "Isaac Iyiola",
+        option2: "Isaac Oluwatoyin",
+        option3: "Taofik Olamilekan",
+    },
+    {
+        fullname: "Isah Hassan",
+        url: "images/Isah_Hassan.jpeg",
+        option1: "Isah Hassan",
+        option2: "Ajiboye Isah",
+        option3: "Aliyu Hassan",
+    },
+    {
+        fullname: "Marvelous Efebe",
+        url: "images/Marvelous_Efebe.jpeg",
+        option1: "Solabi Marvelous",
+        option2: "Marvelous Efebe",
+        option3: "Olamilekan Marvelous",
+    },
+    {
+        fullname: "Maymounah Mogaji",
+        url: "images/Maymounah_Mogaji.jpeg",
+        option1: "Hamzat Mogaji",
+        option2: "Maymounah Ganiyat",
+        option3: "Maymounah Mogaji",
+    },
+    {
+        fullname: "Modinat Adenike",
+        url: "images/Modinat_Adenike.jpeg",
+        option1: "Zainab Modinat",
+        option2: "Modinat Adenike",
+        option3: "Maymounah Mogaji"
+    },
+    {
+        fullname: "Moshood Sanusi",
+        url: "images/Moshood_Sanusi.jpeg",
+        option1: "Moshood Sanusi",
+        option2: "Akinnukawe Sanusi",
+        option3: " Moshood Sanni"
+    },
+    {
+        fullname: "Musa Ojo",
+        url: "images/musa_ojo.jpeg",
+        option1: "Musa Muideen",
+        option2: "Musa Ojo",
+        option3: "Ahmed Ojo"
+    },
+    {
+        fullname: "Mustapha Abdulkareem",
+        url: "images/Mustapha_Abdulkareem.jpeg",
+        option1: "Aliyu Isa",
+        option2: "Abdulhafeez Mustapha",
+        option3: "Mustapha Abdulkareem",
+    },
+    {
+        fullname: "Nweke Chidi",
+        url: "images/NWEKE_CHIDI.jpeg",
+        option1: "Nweke Chidi",
+        option2: "Chidi Confidence",
+        option3: "Nweke Chikwuemeka"
+    },
+    {
+        fullname: "Nwigwe Christiana",
+        url: "images/Nwigwe Christiana.jpeg",
+        option1: "Nwigwe Joy",
+        option2: "Nwigwe Christiana",
+        option3: "Udeme Christiana",
+    },
+    {
+        fullname: "Nwokporo Chukwuebuka",
+        url: "images/Nwokporo_Chukwuebuka.jpg",
+        option1: "Nwokporo Chukwuebuka",
+        option2: "Odezulu Augustine",
+        option3: "Nwokporo Chukwuemeka"
+    },
+    {
+        fullname: "Odezulu Augustine",
+        url: "images/Odezulu_Augustine.jpg",
+        option1: "Augutine Oluwatoyin",
+        option2: "Nwokporo Odezulu",
+        option3: "Odezulu Augustine",
+    },
+    {
+        fullname: "Odinaka Emmanuel",
+        url: "images/ODINAKA_EMMANUEL.jpeg",
+        option1: "Emmanuel Ajibade",
+        option2: "Odinaka Emmanuel",
+        option3: "Odezulu Emmanuel"
+    },
+    {
+        fullname: "Offiong Joy",
+        url: "images/offiong_joy.jpeg",
+        option1: "Effiong Joy",
+        option2: "Eko Joy",
+        option3: "Offiong Joy",
+    },
+    {
+        fullname: "Olamilekan Akinbo",
+        url: "images/Olamilekan_Daniel.jpg",
+        option1: "Olamilekan Efebe",
+        option2: "Taofik Olamilekan",
+        option3: "Olamilekan Akinbo",
+    },
+    {
+        fullname: "Olayiwola Ganiyat",
+        url: "images/OLAYIWOLA_GANIYAT.jpg",
+        option1: "Olayiwola Ganiyat",
+        option2: "Ganiyat Adamu",
+        option3: "Hauwa Adenike"
+    },
+    {
+        fullname: "Olorunsogo Banwo",
+        url: "images/Olorunsogo_Banwo.jpg",
+        option1: "Banwo Augustine",
+        option2: "Olorunsogo Banwo",
+        option3: "Samuel Ojedele"
+    },
+    {
+        fullname: "Oluwatosin Iyiola",
+        url: "images/Oluwatosin_Iyiola.jpeg",
+        option1: "Oluwatoyin Iyiola",
+        option2: "Iyiola Joshua",
+        option3: "Oluwatosin Iyiola",
+    },
+    {
+        fullname: "Oma Raymond",
+        url: "images/Oma_Raymond.jpg",
+        option1: "Oma Raymond",
+        option2: "Zainab Raymond",
+        option3: "Oma Adenike"
+    },
+    {
+        fullname: "Orji Theodora",
+        url: "images/Orji_Theodora_Nneoma.JPG",
+        option1: "Oji Theodora",
+        option2: "Nwigwe Christiana",
+        option3: "Orji Theodora",
+    },
+    {
+        fullname: "Raymond Olugu",
+        url: "images/Raymond_Olugu.jpeg",
+        option1: "Raymond Olugu",
+        option2: "Raymond Okiti",
+        option3: "Godswill Raymond"
+    },
+    {
+        fullname: "Reina Mmesoma",
+        url: "images/Reina_Mmesoma.jpg",
+        option1: "Offiong Reina",
+        option2: "Reina Mmesoma",
+        option3: "Reina Nwankwo"
+    },
+    {
+        fullname: "Richardson Okiti",
+        url: "images/Richardson_Okiti.jpg",
+        option1: "Wilson Okiti",
+        option2: "Richarlison Okiti",
+        option3: "Richardson Okiti",
+    },
+    {
+        fullname: "Rotimi Jemilat",
+        url: "images/Rotimi_Jemilat.JPG",
+        option1: "Olayiwola Ganiyat",
+        option2: "Rotimi Zainab",
+        option3: "Rotimi Jemilat",
+    },
+    {
+        fullname: "Samuel Ojedele",
+        url: "images/samuel ojedele.jpg",
+        option1: "Samuel George",
+        option2: "Samuel Ojedele",
+        option3: "Oluwatosin Ojedele"
+    },
+    {
+        fullname: "Solabi Abraham",
+        url: "images/Solabi_Abraham.jpeg",
+        option1: "Abraham Bassey",
+        option2: "Samuel Abraham",
+        option3: "Solabi Abraham",
+    },
+    {
+        fullname: "Susan Odii",
+        url: "images/Susan_Odii.jpg",
+        option1: "Susan Odii",
+        option2: "Cynthia Odii",
+        option3: "Nwigwe Susan"
+    },
+    {
+        fullname: "Taofik Olamilekan",
+        url: "images/Taofik_Olamilekan.jpg",
+        option1: "Mustapha Toafik",
+        option2: "Taofik Olamilekan",
+        option3: "Mustapha Akinbo"
+    },
+    {
+        fullname: "Uche Ogwumike",
+        url: "images/Uche_Ogwumike.jpg",
+        option1: "Eche Ogwumike",
+        option2: "Eduvie Udusegbe",
+        option3: "Uche Ogwumike",
+    },
+    {
+        fullname: "Udeme Eko",
+        url: "images/Udeme_Eko.jpg",
+        option1: "Susan Udeme",
+        option2: "Udeme Eko",
+        option3: "Orji Udeme"
+    },
+    {
+        fullname: "Udonte George",
+        url: "images/Udonte_George.jpeg",
+        option1: "George Raymond",
+        option2: "Augustine George",
+        option3: "Udonte George",
+    },
+    {
+        fullname: "Ukaoha Chizoba",
+        url: "images/Ukaoha_Chizoba.jpg",
+        option1: "Reina Ukaoha",
+        option2: "Ukaoha Chizoba",
+        option3: "Susan Ukaoha"
+    },
+    {
+        fullname: "Victor Bassey",
+        url: "images/victor_bassey.jpg",
+        option1: "Victor Basil",
+        option2: "Victor Barnabas",
+        option3: "Victor Bassey",
+    },
+    {
+        fullname: "Wilson Oyem",
+        url: "images/Wilson_Oyem.jpeg",
+        option1: "Wilson Oyem",
+        option2: "Godswill Oyem",
+        option3: "Owem William"
+    },
+    {
+        fullname: "Yusuf Olanrewaju",
+        url: "images/Yusuf_Olanrewaju.jpg",
+        option1: "Yusuf Olamilekan",
+        option2: "Yusuf Olanrewaju",
+        option3: "Musa Olaniyi"
+    },
+    {
+        fullname: "Zainab Momoh",
+        url: "images/Zainab_Momoh.JPG",
+        option1: "Zainab Alaba",
+        option2: "Zainab Momoh",
+        option3: "Susan Momoh"
+    }
+]
+
+
+// load sections and buttons
+// landing section
+const landing = document.getElementById('landing')
+// landing.style.display = "none"
+const LandingPlayBtn = document.getElementById('landing_play')
+
+// rules section
+const rules = document.getElementById('rules')
+rules.style.display = "none"
+const rulesPlayBtn = document.getElementById('game_start')
+
+// play section
+const play = document.getElementById('play')
+play.style.display = "none"
+const nextBtn = document.getElementById('next')
+
+// result section
+const result = document.getElementById('result')
+result.style.display = "none"
+const replay = document.getElementById('replay')
+
+// switch page display when button is clicked
+
+
+// load picture screen and options from play section
+
+const imgDisplay = document.getElementById('display')
+const optionOne = document.getElementById('option_one')
+const optionTwo = document.getElementById('option_two')
+const optionThree = document.getElementById('option_three')
+
+// both options
+let options = document.querySelectorAll('.option')
+
+// timer and image counter
+const countDowntime = document.getElementById('timer')
+const imageNum = document.getElementById('imageNum')
+
+// load result page items
+let medal = document.querySelector('.medal')
+let finalCorrectTotal = document.getElementById('total')
+let remark = document.getElementById('remark')
+
+
+let index = 0
+let timer = 0
+let interval = 0
+// this will be the score counter
+let CorrectTotal = 0
+
+// user answers stored here
+let userAns = undefined
+
+// create Timer
+const countDown = () => {
+    if (timer === 20) {
+        // clearInterval(interval);
+        endGame()
+    } else {
+        timer++
+        countDowntime.innerText = timer;
+    }
+}
+
+let getFellows = []
+
+const loadData = () => {
+
+    // let i = 0
+    while (getFellows.length !== 10) {
+        let rand = Math.floor(Math.random() * fellows.length)
+
+        if (!getFellows.includes(fellows[rand])) {
+            getFellows.push(fellows[rand])
+        }
+    }
+
+    imageNum.textContent = index + 1
+    imgDisplay.src = getFellows[index].url
+
+    optionOne.textContent = getFellows[index].option1
+    optionTwo.textContent = getFellows[index].option2
+    optionThree.textContent = getFellows[index].option3
+
+
+}
+
+loadData()
+
+
+// What happens when the play button on the landing section is clicked
+LandingPlayBtn.addEventListener('click', () => {
+
+    landing.style.display = "none"
+    rules.style.display = "flex"
+    play.style.display = "none"
+    result.style.display = "none"
+})
+
+// What happens when the play button on the rules section is clicked
+rulesPlayBtn.addEventListener('click', () => {
+
+    landing.style.display = "none"
+    rules.style.display = "none"
+    result.style.display = "none"
+    play.style.display = "flex"
+
+    // setinterval
+    interval = setInterval(countDown, 1000);
+    loadData()
+
+})
+
+
+// check correct answer
+options.forEach((option) => {
+    option.addEventListener('click', () => {
+        option.classList.add("active")
+        if (option.textContent == getFellows[index].fullname) {
+            CorrectTotal++
+        } else {
+            CorrectTotal += 0
+        }
+
+        // de-activate click
+        for (i = 0; i <= 3; i++) {
+            options[i].classList.add("disabled");
+        }
+    })
+})
+
+nextBtn.addEventListener("click", () => {
+    // 
+    if (index !== getFellows.length - 1) {
+        index++
+        options.forEach(removeActive => {
+            removeActive.classList.remove("active");
+        })
+        loadData()
+
+    } else {
+        index = 0
+
+        clearInterval(interval);
+        play.style.display = "none"
+        finalCorrectTotal.textContent = CorrectTotal
+
+        endGame()
+
+    }
+    for (i = 0; i <= 3; i++) {
+        options[i].classList.remove("disabled");
+    }
+
+})
+
+
+const endGame = () => {
+    clearInterval(interval);
+    play.style.display = "none"
+    finalCorrectTotal.textContent = CorrectTotal
+
+
+    if (CorrectTotal >= 3 && CorrectTotal < 5) {
+        medal.textContent = "🥉"
+        remark.textContent = "👍🏽 You try, we will give you plastic 👍🏽"
+    } else if (CorrectTotal >= 5 && CorrectTotal < 7) {
+        medal.textContent = "🥈"
+        remark.textContent = "👍🏽👍🏽 You try, you deserve some accolades 👍🏽👍🏽"
+    } else if (CorrectTotal >= 7 && CorrectTotal < 10) {
+        medal.textContent = "🥇"
+        remark.textContent = "🙌🏽🙌🏽🙌🏽 Oshey Familiar Spirit 🙌🏽🙌🏽🙌🏽"
+    } else if (CorrectTotal == 10) {
+        medal.textContent = "🏆"
+        remark.textContent = "🤝🏽🤝🏽🤝🏽 Give it up for Facebook 🤝🏽🤝🏽🤝🏽"
+    } else {
+        medal.textContent = "🎈"
+        remark.textContent = "🚶🏽 Dey try come class 🚶🏽‍♂️"
+    }
+
+    result.style.display = "flex"
+
+
+}
+
+// reload button
+const reloadBtn = document.getElementById('replay')
+reloadBtn.addEventListener('click', () => {
+    location.reload()
+})
